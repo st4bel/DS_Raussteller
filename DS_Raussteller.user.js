fc94c4f2c36df4cbcd788a42064c74aad2d31a56
Max Stapelfeld <nasak.o@hotmail.de>
2016-12-16 19:18:00 +0100

    initial add

---
+++ DS_Raussteller.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name        DS_Raussteller
+// @namespace   de.die-staemme
+// @version     0.1
+// @description Stellt Truppen in angegriffenen Dörfern automatisch raus, und bricht die Angriffe ab.
+// @grant       GM_getValue
+// @grant       GM_setValue
+// @grant       unsafeWindow
+// @match       https://*.die-staemme.de/game.php?*screen=am_*
+// @include     https://*.die-staemme.de/game.php?*screen=am_*
+// @include     https://*.die-staemme.de/game.php?*screen=accountmanager*
+// @copyright   2016+, the stabel, git
+// @downloadURL -
+// ==/UserScript==
+
+
+$(function(){
+    
+});
